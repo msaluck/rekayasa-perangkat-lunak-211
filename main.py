1d9608cc050ef8aed6c856b46a4f0ac73e53372e
iesteaa <sedayuhendrick18@gmail.com>
2021-09-14 06:59:34 +0700

    Pak saya konfilik sama ucok

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 print('Daftar Hadir')
 # isi dengan nama dan nim
-print ("Muhammad Alifudin")
-print ("H1A019029")
-print ("Ngelamun")
+print ("Nama saya Muhammad Hendrick Sedayu")
+print ("NIM saya H1A)19060")
+print ("Hobi Saya Bergerak")
