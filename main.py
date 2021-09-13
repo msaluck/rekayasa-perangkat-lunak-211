9c070f87d63100c520090bb15330326b2438a30e
KoentjoroFIG <fajrul.koentjoro@mhs.unsoed.ac.id>
2021-09-13 21:10:49 +0700

    Konflik Fajrul

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 print('Daftar Hadir')
 # isi dengan nama dan nim
-<<<<<<< HEAD
-print('Naufal Syafiq Safikri')
-print('H1A019039')
-print('Nonton Anime')
-=======
-print('Fikra')
-print('H1A019083')
-print('Nonton Kamen Rider')
->>>>>>> ba46bf54fe6e154ed3f74f6ae000d273055977a4
+print ("Fajrul Iman Giat Koentjoro")
+print ("H1A019032")
+print ("Maido")
