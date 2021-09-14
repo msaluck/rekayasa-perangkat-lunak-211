48502fae8f4894cc5521bf18efe0537c03e2ab39
guteng083 <stevanusarlian@gmail.com>
2021-09-14 08:04:41 +0700

    Edit Identitas NIM 35

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 print('Daftar Hadir')
 # isi dengan nama dan nim
-<<<<<<< HEAD
 print ("Stefanus Arlian Situmeang")
 print ("H1A019035")
 print ("Nonton Koreaan")
-=======
-print ("Nama saya Muhammad Hendrick Sedayu")
-print ("NIM saya H1A)19060")
-print ("Hobi Saya Bergerak")
->>>>>>> 1d9608cc050ef8aed6c856b46a4f0ac73e53372e
