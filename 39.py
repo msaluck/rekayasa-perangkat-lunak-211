399eae0536665e2201c3a5217c1e6b65fa402fa1
8lafuan8 <naufal.safikri@mhs.unsoed.ac.id>
2021-09-14 09:02:38 +0700

    memperbaiki konflik yang terjadi dengan memilih branch2

--- 39.py
+++ 39.py
@@ -1,9 +1,4 @@
 print('Naufal Syafiq Safikri')
 print('NIM: H1A019039')
-<<<<<<< HEAD
-#menambah mobi tanpa branch (jalur mains)
-print('Menonton Anime adalah hobby saya')
-=======
 #menambah hobi dengan branch2
 print('Hobinya nonton Anime')
->>>>>>> branch2
